783d87543ff2cce34635b5e339bac8a9fa1b1bec
KarlaD7 <karlaiulia.durdun@ulbsibiu.ro>
2026-03-10 09:22:57 +0200

    Rezolvare problema 2.5.2 lab2

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,18 +1,43 @@
 package ro.ulbs.proiectaresoftware.students;
 
-public class Application {public static void main(String[] args) {
-    Student s1 = new Student(112, "Ioan", "Popa", "TI21/1");
-    Student s2 = new Student(112, "Maria", "Oprea", "TI21/1");
-    Student s3 = new Student(120, "Alis",  "Popa", "TI21/2");
-    Student s4 = new Student(122, "Mihai", "Vecerdea", "TI22/1");
-    Student s5 = new Student(122, "Eugen", "Uritescu", "TI22/2");
+import java.util.ArrayList;
+import java.util.List;
 
-    System.out.println(String.format("%15s %15s %15s %15s", "numar matricol", "prenume", "nume", "formatieDeStudiu"));
+public class Application {
+    public static void main(String[] args) {
 
-    System.out.println(s1);
-    System.out.println(s2);
-    System.out.println(s3);
-    System.out.println(s4);
-    System.out.println(s5);
-}
+        // a) Crearea unei liste din lista de instante Student
+        List<Student> listaStudenti = new ArrayList<>();
+
+        listaStudenti.add(new Student(112, "Ioan", "Popa", "TI21/1"));
+        listaStudenti.add(new Student(112, "Maria", "Oprea", "TI21/1"));
+        listaStudenti.add(new Student(120, "Alis", "Popa", "TI21/2"));
+        listaStudenti.add(new Student(122, "Mihai", "Vecerdea", "TI22/1"));
+        listaStudenti.add(new Student(122, "Eugen", "Uritescu", "TI22/2"));
+
+        System.out.println("lista de studenti");
+        for (Student s : listaStudenti) {
+            System.out.println(s);
+        }
+
+        // b) si c) Verificarea prezentei studentilor
+        Student sCautantB = new Student(120, "Alis", "Popa", "TI21/2");
+        Student sCautantC = new Student(112, "Maria", "Popa", "TI21/1");
+
+        System.out.println("\nRezultate verificare:");
+        System.out.println("Studentul Alis Popa (b) este prezent? " + existaStudent(listaStudenti, sCautantB));
+        System.out.println("Studentul Maria Popa (c) este prezent? " + existaStudent(listaStudenti, sCautantC));
+    }
+
+
+    public static boolean existaStudent(List<Student> lista, Student s) {
+        for (Student studentDinLista : lista) {
+            if (studentDinLista.getPrenume().equals(s.getPrenume()) &&
+                    studentDinLista.getNume().equals(s.getNume()) &&
+                    studentDinLista.getFormatieDeStudiu().equals(s.getFormatieDeStudiu())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
